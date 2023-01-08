8b9af2a5086e29a8c89b4b55694a1f6ae1f4f45f
KnDevTeam <kndevelopmentteam@gmail.com>
2023-01-08 05:07:40 +0000

    First commit of new bot

--- main.py
+++ main.py
@@ -1,33 +1,22 @@
 import logging
-import os
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
+from config import *
 
 
-TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
-
-# webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-
-# webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
-
 
 async def on_startup(dispatcher):
+    logging.warning('Connected... Ready to work. ')
     await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
 
 async def on_shutdown(dispatcher):
+    logging.warning('Bot STOPPED! Shutting down webhook connection')
     await bot.delete_webhook()
 
 
