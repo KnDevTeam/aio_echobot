798dcc52dd7e86d6e75348ea18889de90b42368b
KnDevTeam <kndevelopmentteam@gmail.com>
2023-01-08 03:33:41 +0000

    First commit of new bot

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ API_TOKEN = "5666366626:AAFYT9yDcGExQz5ILdLCGPEg99RF8X7dZNg"
 
 # webhook settings
 WEBHOOK_HOST = 'https://aio-echo-bot.herokuapp.com'
-WEBHOOK_PATH = f'webhook/{API_TOKEN}'
+WEBHOOK_PATH = f'/webhook/{API_TOKEN}'
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'  # or ip
-WEBAPP_PORT = 5000 # original 3001
+WEBAPP_PORT = 8443 # original 3001
